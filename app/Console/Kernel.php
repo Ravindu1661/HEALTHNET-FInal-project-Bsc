c18cb30b65d70825b16133167a058b270f92cecc
Ravindu1661 <rravindu989@gmail.com>
2026-03-01 04:47:33 +0530

    add profile

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,29 +4,23 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\SendMedicineReminderNotifications;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
+    protected $commands = [
+        SendMedicineReminderNotifications::class,
+    ];
+
+    protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // හරියටම minute-by-minute check
+        $schedule->command('reminders:send')->everyMinute();
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
-
         require base_path('routes/console.php');
     }
 }
